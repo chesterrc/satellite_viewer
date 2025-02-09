e48f69f5fe5f09be77227acf13ef3800d22f1010
chesterrc <chezramosc@gmail.com>
2025-02-08 23:36:06 -0500

    added sscws import and get request for iss

--- src/satellite_viewer_server/satellite_viewer_server/views.py
+++ src/satellite_viewer_server/satellite_viewer_server/views.py
@@ -1,7 +1,14 @@
-from flask import Flask
+import datetime
+from flask import Flask, request
+from sscws.sscws import SscWs 
+
 
 app = Flask(__name__)
+ssc = SscWs()
 
-@app.route("/")
-def hello_world():
-    return "fuck"
+
+@app.get("/", method=["GET"])
+def home():
+    time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    req = request.get_json(ssc.get_locations(["iss"]), time)
+    return req
